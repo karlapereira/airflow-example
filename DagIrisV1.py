1f02785012f73eb85663fb43e8e518d12277044f
karlapereira <karla.pereiraa.kp@gmail.com>
2023-11-23 00:23:31 -0300

    feat: api

--- DagIrisV1.py
+++ DagIrisV1.py
@@ -5,39 +5,42 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.utils.task_group import TaskGroup
 
 pathScript = "./etl_scripts"
-pathIris = "./etl_scripts/featurestore/iris.txt"
-pathEncoder ="./etl_scripts/featurestore/irisEncoder.txt"
+pathIris =  "./etl_scripts/featurestore/iris.txt"
+pathEncoder = "./etl_scripts/featurestore/irisEncoder.txt"
 
 default_args = {
-    "owner": "teste",
-    "depends_on_past": False,
-    "start_date": datetime(2019, 1, 1),
-    "retries": 0
-}
+   'owner': 'teste',
+   'depends_on_past': False,
+   'start_date': datetime(2019, 1, 1),
+   'retries': 0,
+   }
 
 with DAG(
-    "dag-pipeline-iris-aula-v1",
-    schedule_interval=timedelta(minutes=10),
-    catchup=False,
-    default_args=default_args
-) as dag:
+   'dag-pipeline-iris-aula-v1',
+   schedule_interval=timedelta(minutes=10),
+   catchup=False,
+   default_args=default_args
+   ) as dag:
+
     start = DummyOperator(task_id="start")
+
     with TaskGroup("etl", tooltip="etl") as etl:
+        
         t1 = BashOperator(
             dag=dag,
-            task_id="download_dataset",
+            task_id='download_dataset',
             bash_command="""
             cd {0}/featurestore
-            curl -o iris.txt https://archive.ics.uci.edu/ml/machine-learning-
-            databases/iris/iris.data
+            curl -o iris.txt  https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data
             """.format(pathScript)
         )
+
         [t1]
 
     with TaskGroup("preProcessing", tooltip="preProcessing") as preProcessing:
         t2 = BashOperator(
             dag=dag,
-            task_id="encoder_dataset",
+            task_id='encoder_dataset',
             bash_command="""
             cd {0}
             python etl_preprocessing.py {1} {2}
@@ -45,5 +48,6 @@ with DAG(
         )
         [t2]
 
-    end = DummyOperator(task_id="end")
+
+    end = DummyOperator(task_id='end')
     start >> etl >> preProcessing >> end
--- DagIrisV2.py
+++ DagIrisV2.py
@@ -4,43 +4,45 @@ from airflow.operators.dummy import DummyOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.utils.task_group import TaskGroup
 
-
 pathScript = "./etl_scripts"
-pathIris = "./featurestore/iris.txt"
+pathIris =  "./etl_scripts/featurestore/iris.txt"
 pathEncoder = "./etl_scripts/featurestore/irisEncoder.txt"
 pathDeploy = "./deployApi/apiIris"
 pathModel = "./deployApi/apiIris/model.pkl"
 
 default_args = {
-    "owner": "teste",
-    "depends_on_past": False,
-    "start_date": datetime(2019, 1, 1),
-    "retries": 0,
-}
+   'owner': 'teste',
+   'depends_on_past': False,
+   'start_date': datetime(2019, 1, 1),
+   'retries': 0,
+   }
 
 with DAG(
-    "dag-pipeline-iris-aula-v2",
-    schedule_interval=timedelta(minutes=10),
-    catchup=False,
-    default_args=default_args
-) as dag:
+   'dag-pipeline-iris-aula-v2',
+   schedule_interval=timedelta(minutes=10),
+   catchup=False,
+   default_args=default_args
+   ) as dag:
+
     start = DummyOperator(task_id="start")
+
     with TaskGroup("etl", tooltip="etl") as etl:
+        
         t1 = BashOperator(
             dag=dag,
-            task_id="download_dataset",
+            task_id='download_dataset',
             bash_command="""
             cd {0}/featurestore
-            curl -o iris.txt https://archive.ics.uci.edu/ml/machine-learning-
-            databases/iris/iris.data
+            curl -o iris.txt  https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data
             """.format(pathScript)
         )
+
         [t1]
 
     with TaskGroup("preProcessing", tooltip="preProcessing") as preProcessing:
         t2 = BashOperator(
             dag=dag,
-            task_id="encoder_dataset",
+            task_id='encoder_dataset',
             bash_command="""
             cd {0}
             python etl_preprocessing.py {1} {2}
@@ -51,19 +53,18 @@ with DAG(
     with TaskGroup("model", tooltip="model") as model:
         t3 = BashOperator(
             dag=dag,
-            task_id="modelo",
+            task_id='modelo',
             bash_command="""
             cd {0}
             python ml_sklearn.py {1} {2} {3} {4} {5}
-            """.format(pathScript,pathEncoder, "IrisClassificacao", "ModeloIris",
-            2, 0)
+            """.format(pathScript,pathEncoder, "IrisClassificacao", "ModeloIris", 2, 0)
         )
         [t3]
 
     with TaskGroup("validate", tooltip="validate") as validate:
         t4 = BashOperator(
             dag=dag,
-            task_id="validate",
+            task_id='validate',
             bash_command="""
             cd {0}
             python validateModel.py {1} {2}
@@ -71,5 +72,5 @@ with DAG(
         )
         [t4]
 
-    end = DummyOperator(task_id="end")
+    end = DummyOperator(task_id='end')
     start >> etl >> preProcessing >> model >> validate >> end
